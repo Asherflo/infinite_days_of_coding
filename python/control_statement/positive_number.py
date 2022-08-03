49d3432cf467bebc7486add00f2f1d2038e7c576
Asherflo <aniyikayetemitopeflorence@gmail.com>
2022-08-03 06:18:48 +0100

    breakfast on bed

---
+++ python/control_statement/positive_number.py
@@ -0,0 +1,5 @@
+number = int(input("enter a number"))
+if number > 0:
+    print("the number is a positive number")
+else:
+    print("the number is not a positive number")
---
+++ python/control_statement/vowel_checker.py
@@ -0,0 +1,6 @@
+ch = input("Enter any character ")
+vowel ="aeiouAEIOU"
+if ch in vowel:
+    print("the character is vowel")
+else:
+    print("the character is not vowel")
